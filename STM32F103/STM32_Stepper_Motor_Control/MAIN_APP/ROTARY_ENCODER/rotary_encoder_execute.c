553c86f9aa7a9ba62d89ab24209b35a897a3e632
alex <your_email@whatever.com>
2021-09-15 16:37:36 +0300

    [REFACTOR] relocation part of the code from "Execute_RotaryEncoder_Send_Data_To_Motor_Thread()" to "Execute_RotaryEncoder_Parse_Data()" function

--- STM32F103/STM32_Stepper_Motor_Control/MAIN_APP/ROTARY_ENCODER/rotary_encoder_execute.c
+++ STM32F103/STM32_Stepper_Motor_Control/MAIN_APP/ROTARY_ENCODER/rotary_encoder_execute.c
@@ -19,22 +19,7 @@ ROTARY_ENCODER_STATUS_t Execute_RotaryEncoder_Waiting_Data(ROTARY_ENCODER_t *rot
 ROTARY_ENCODER_STATUS_t Execute_RotaryEncoder_Parse_Data(ROTARY_ENCODER_t *rotary_encoder)
 {
   ROTARY_ENCODER_STATUS_t execute_status = ROTARY_ENCODER_STATUS_PARSE_ENCODER_SENSOR_DATA_ERROR;
-
-  execute_status = ROTARY_ENCODER_STATUS_PARSE_ENCODER_SENSOR_DATA_OK;
-
-  return execute_status;
-}
-
-ROTARY_ENCODER_STATUS_t Execute_RotaryEncoder_Send_Data_To_Motor_Thread(ROTARY_ENCODER_t *rotary_encoder)
-{
-  ROTARY_ENCODER_STATUS_t execute_status = ROTARY_ENCODER_STATUS_SEND_DATA_TO_MOTOR_THREAD_ERROR;
-  static int32_t queue_tx_step;
-  
-  
-  
-  
-  
-  
+  static int32_t queue_tx_step = 0;
   
   
   
@@ -61,6 +46,55 @@ ROTARY_ENCODER_STATUS_t Execute_RotaryEncoder_Send_Data_To_Motor_Thread(ROTARY_E
    
   rotary_encoder->queue_motor_set_parametrs.counter = rotary_encoder->queue_motor_set_parametrs.counter + 1;
   rotary_encoder->mptr->counter = rotary_encoder->queue_motor_set_parametrs.counter;
+  /* osMessagePut(MsgBox, (uint32_t)rotary_encoder->mptr, osWaitForever);  // Send Message */
+  taskEXIT_CRITICAL();
+  
+  
+  
+  
+  
+
+  execute_status = ROTARY_ENCODER_STATUS_PARSE_ENCODER_SENSOR_DATA_OK;
+
+  return execute_status;
+}
+
+ROTARY_ENCODER_STATUS_t Execute_RotaryEncoder_Send_Data_To_Motor_Thread(ROTARY_ENCODER_t *rotary_encoder)
+{
+  ROTARY_ENCODER_STATUS_t execute_status = ROTARY_ENCODER_STATUS_SEND_DATA_TO_MOTOR_THREAD_ERROR;
+  // static int32_t queue_tx_step = 0;
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  taskENTER_CRITICAL();
+  /*if(rotary_encoder->queue_motor_set_parametrs.Period_0 <= 2000)
+  {
+    queue_tx_step = 100; // NEMA17-800 NEMA23-2000
+  }
+  if(rotary_encoder->queue_motor_set_parametrs.Period_0 >= 6000)
+  {
+    queue_tx_step = -100; // NEMA17-3500 NEMA23-6000
+  }
+  rotary_encoder->queue_motor_set_parametrs.Period_0 += queue_tx_step;
+    
+  rotary_encoder->mptr->Enable_0 = rotary_encoder->queue_motor_set_parametrs.Enable_0;
+  rotary_encoder->mptr->Enable_1 = rotary_encoder->queue_motor_set_parametrs.Enable_1;
+  rotary_encoder->mptr->Direction_0 = rotary_encoder->queue_motor_set_parametrs.Direction_0;
+  rotary_encoder->mptr->Direction_1 = rotary_encoder->queue_motor_set_parametrs.Direction_1;
+  rotary_encoder->mptr->Period_0 = rotary_encoder->queue_motor_set_parametrs.Period_0;
+  rotary_encoder->mptr->Period_1 = rotary_encoder->queue_motor_set_parametrs.Period_1;
+  rotary_encoder->mptr->DutyCycle_0 = rotary_encoder->queue_motor_set_parametrs.DutyCycle_0;
+  rotary_encoder->mptr->DutyCycle_1 = rotary_encoder->queue_motor_set_parametrs.DutyCycle_1;
+   
+  rotary_encoder->queue_motor_set_parametrs.counter = rotary_encoder->queue_motor_set_parametrs.counter + 1;
+  rotary_encoder->mptr->counter = rotary_encoder->queue_motor_set_parametrs.counter;
+  */
   osMessagePut(MsgBox, (uint32_t)rotary_encoder->mptr, osWaitForever);  // Send Message
   taskEXIT_CRITICAL();
 

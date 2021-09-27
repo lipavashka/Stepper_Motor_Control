2806f6eb0fa781d46260abe0c525d682fe696b37
alex <your_email@whatever.com>
2021-09-27 16:11:53 +0300

    [FEAT] Added "LED" type

---
+++ STM32F103/STM32F103RCT6_Stepper_Motor_Control/MAIN_APP/LEDs/main_app_led.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "stdbool.h"
+
+void LED_Step_Motor_0(bool value);
+void LED_Step_Motor_1(bool value);
+void LED_Encoder(bool value);
+void LED_LCD(bool value);
+void LED_Button(bool value);
+void LED_EEPROM(bool value);
+void LED_UART1(bool value);
+void LED_UART3(bool value);
